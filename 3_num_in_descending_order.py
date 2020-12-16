cec0623a277b953771913ae5236fd53874d0dcdd
SIMRAN ARORA <74872611+SimranArora2@users.noreply.github.com>
2020-12-16 19:08:36 +0530

    Create 3_num_in_descending_order.py

---
+++ 3_num_in_descending_order.py
@@ -0,0 +1,17 @@
+a=int(input("enter 1st no.:"))
+b=int(input("enter 2nd no.:"))
+c=int(input("enter 3rd no.:"))
+if a==b and b<c :
+      print(a,"=",b,">",c)
+elif a>b and b==c :
+      print(a,">",b,c)
+elif a>b and b>c :
+      print(a,">",b,">",c)
+elif b>c and c>a :
+      print(b,">",c,">",">",a)
+elif c>b  and b>a :
+      print(c,">",b,">",a)
+elif a==c and c>b :
+      print(a,c,">",b)
+elif a==b==c :
+      print(a,"=",b,"=",c)
